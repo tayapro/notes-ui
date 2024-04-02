eb209cefe1d9ad4a251c647f098ee3ff34741f68
Taisia Simakova <stipaxa@gmail.com>
2024-04-02 16:56:40 +0100

    Reset input values when modal windows closed

--- src/store/notesStore.js
+++ src/store/notesStore.js
@@ -1,23 +1,10 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { jwtDecode } from 'jwt-decode'
-import idApi from '../api/idApi.js'
-// import notesApi from '../api/notesApi.js'
-import notesMiddleware from '../middleware/notes.js'
 
 export const useStore = defineStore('notes', () => {
     const notes = ref([])
     const filter = ref('')
     const isLoggedIn = ref(false)
-
-    function getNoteByID(id) {
-        for (let n of notes.value) {
-            if (n.id === id) {
-                return n
-            }
-        }
-        return undefined
-    }
 
     return {
         notes,
